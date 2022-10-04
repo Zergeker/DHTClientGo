7c7108fa1c6b4fe437f9452cf7c2533249a83376
Zergeker <zergec@yandex.ru>
2022-10-04 10:41:02 +0200

    Client works with multiple hosts

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ import (
 )
 
 func main() {
-	ip := os.Args[1]
+	ipArray := [16]string{"10.1.2.233", "10.1.2.197", "10.1.2.184", "10.1.2.183", "10.1.2.182", "10.1.2.181", "10.1.2.180", "10.1.2.179", "10.1.2.178", "10.1.2.177", "10.1.2.161", "10.1.2.143", "10.1.2.142", "10.1.2.123", "10.1.2.122", "10.1.2.121"}
 
 	rand.Seed(time.Now().Unix())
+
+	nodesCount, _ := strconv.Atoi(os.Args[1])
 
 	flag := true
 	successfulCounter := 0
@@ -23,11 +25,12 @@ func main() {
 	client := &http.Client{}
 
 	for flag == true {
+		ip := rand.Intn(nodesCount)
 		key := rand.Int()
 		value := rand.Int()
 
-		reqPut, err := http.NewRequest(http.MethodPut, "http://"+ip+":58346/storage/"+strconv.Itoa(key), bytes.NewBuffer([]byte(strconv.Itoa(value))))
-		reqGet, err := http.NewRequest(http.MethodGet, "http://"+ip+":58346/storage/"+strconv.Itoa(key), nil)
+		reqPut, err := http.NewRequest(http.MethodPut, "http://"+ipArray[ip]+":58346/storage/"+strconv.Itoa(key), bytes.NewBuffer([]byte(strconv.Itoa(value))))
+		reqGet, err := http.NewRequest(http.MethodGet, "http://"+ipArray[ip]+":58346/storage/"+strconv.Itoa(key), nil)
 
 		_, err = client.Do(reqPut)
 
