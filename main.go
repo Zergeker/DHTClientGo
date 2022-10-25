d82681754926691dee0475dfe3711997956e3999
Zergeker <zergec@yandex.ru>
2022-10-25 16:02:01 +0200

    Changed code for the Assignment 2

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"math/rand"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -11,45 +11,78 @@ import (
 )
 
 func main() {
-	ipArray := [16]string{"10.1.2.233", "10.1.2.197", "10.1.2.184", "10.1.2.183", "10.1.2.182", "10.1.2.181", "10.1.2.180", "10.1.2.179", "10.1.2.178", "10.1.2.177", "10.1.2.161", "10.1.2.143", "10.1.2.142", "10.1.2.123", "10.1.2.122", "10.1.2.121"}
-
-	rand.Seed(time.Now().Unix())
+	ipArray := [50]string{
+		"compute-10-0",
+		"compute-10-1",
+		"compute-10-2",
+		"compute-10-3",
+		"compute-10-4",
+		"compute-10-5",
+		"compute-10-6",
+		"compute-10-7",
+		"compute-10-8",
+		"compute-10-9",
+		"compute-10-10",
+		"compute-10-11",
+		"compute-10-12",
+		"compute-10-13",
+		"compute-10-14",
+		"compute-10-15",
+		"compute-10-16",
+		"compute-10-17",
+		"compute-10-18",
+		"compute-10-19",
+		"compute-10-20",
+		"compute-10-21",
+		"compute-10-22",
+		"compute-10-23",
+		"compute-10-24",
+		"compute-10-25",
+		"compute-10-26",
+		"compute-10-27",
+		"compute-10-28",
+		"compute-10-29",
+		"compute-10-30",
+		"compute-10-31",
+		"compute-10-32",
+		"compute-10-33",
+		"compute-10-34",
+		"compute-10-35",
+		"compute-10-36",
+		"compute-10-37",
+		"compute-10-38",
+		"compute-10-39",
+		"compute-10-40",
+		"compute-10-41",
+		"compute-10-42",
+		"compute-10-43",
+		"compute-10-44",
+		"compute-10-45",
+		"compute-10-46",
+		"compute-10-47",
+		"compute-7-0",
+		"compute-7-1"}
 
 	nodesCount, _ := strconv.Atoi(os.Args[1])
-
-	flag := true
-	successfulCounter := 0
-	errorsCounter := 0
-	time.AfterFunc(5*time.Minute, func() { flag = false })
-
-	client := &http.Client{}
-
-	for flag == true {
-		ip := rand.Intn(nodesCount)
-		key := rand.Int()
-		value := rand.Int()
-
-		reqPut, err := http.NewRequest(http.MethodPut, "http://"+ipArray[ip]+":58346/storage/"+strconv.Itoa(key), bytes.NewBuffer([]byte(strconv.Itoa(value))))
-		reqGet, err := http.NewRequest(http.MethodGet, "http://"+ipArray[ip]+":58346/storage/"+strconv.Itoa(key), nil)
-
-		_, err = client.Do(reqPut)
-
+	port := os.Args[2]
+	joinStart := time.Now()
+	for i := 1; i < nodesCount; i++ {
+		_, err := http.Post("http://"+ipArray[i]+":"+port+"/join?nprime="+ipArray[i-1]+":"+port, "text/plain", bytes.NewReader([]byte("")))
 		if err != nil {
-			errorsCounter++
-		} else {
-			successfulCounter++
-		}
-
-		_, err = client.Do(reqGet)
-
-		if err != nil {
-			errorsCounter++
-		} else {
-			successfulCounter++
+			log.Fatal("Node " + ipArray[i] + " could not join node " + ipArray[i-1])
 		}
 	}
+	joinFinished := time.Since(joinStart)
 
-	fmt.Println("Successful requests: " + strconv.Itoa(successfulCounter))
-	fmt.Println("Failured requests: " + strconv.Itoa(errorsCounter))
-	fmt.Println("Requests per second: " + fmt.Sprintf("%f", (float64(successfulCounter+errorsCounter)/float64(300))))
+	leaveStart := time.Now()
+	for i := nodesCount - 1; i > nodesCount/2; i-- {
+		_, err := http.Post("http://"+ipArray[i]+":"+port+"/leave", "text/plain", bytes.NewReader([]byte("")))
+		if err != nil {
+			log.Fatal("Error occured during node " + ipArray[i] + " leaving")
+		}
+	}
+	leaveFinished := time.Since(leaveStart)
+
+	fmt.Println("Joining time: " + joinFinished.String())
+	fmt.Println("Shrinking time: " + leaveFinished.String())
 }
